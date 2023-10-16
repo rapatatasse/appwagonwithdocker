ba1be276c39c20046e62bffdc5e033550171163f
rapatatasse <platonformation@gmail.com>
2023-10-16 15:22:40 +0200

    step1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @varint = 1
+  
+    @toto = "titit"
+
+    @userconnectmail = current_user.email
+
   end
 end
